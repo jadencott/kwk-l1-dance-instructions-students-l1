a0f95c95960ee99b73603c697ee311290afde6a5
Jaden <jcottorone21405@gmail.com>
2018-06-04 22:01:57 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,12 @@
 
 # Code your methods below
+def lower body
+  puts "plant legs far apart, bend knees slightly and keep posture loose."
+end
+
+def base footwork
+  puts "lift right foot up"
+  puts "return right foot to ground"
+  puts " one skip-step backwards"
+
+
